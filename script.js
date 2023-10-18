a30e7da6dac67f489c9e6336aee02551e4669405
Astha gupta <guptaastha977@gmail.com>
2023-10-18 18:38:00 +0530

    todo with dom

---
+++ script.js
@@ -0,0 +1,25 @@
+let btn= document.querySelector("button");
+let ul= document.querySelector("ul");
+let inp=document.querySelector("input");
+
+btn.addEventListener("click",function(){
+    let item=document.createElement("li");
+    item.innerText=inp.value;
+
+    let delBtn=document.createElement("button");
+    delBtn.innerText="delete";
+    delBtn.classList.add("delete");
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    console.log(inp.value);
+    inp.value="";
+});
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON")
+    {
+        let listItem=event.target.parentElement;
+        listItem.remove();
+        console.log("deleted");
+    }
+})
